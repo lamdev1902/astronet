918460c94d01b1d1644b157136f18fa385a7ac81
truclam1902 <nguyenhongtruclam1@gmail.com>
2023-12-25 10:36:45 +0700

    "three commit"

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'h5Mgomb3pzozLr1yHaHtxjmSPFtNJTq4qnxux0shJHPHYbBa8RoDzDFLjfH19DGx');
+define('SECURE_AUTH_KEY',  '1YK7563GGCjyjCpH7Vcy42ntVKy17Ku5xcERbmJPV9Io8SdDRXWjAEmSuBIW9Tey');
+define('LOGGED_IN_KEY',    'cjyjI0J5RfbCAVFmAFb0vjMTPV6H5XvMcCTWhVLvVDVRqd2PU8EtwyXrJUS1g0Bu');
+define('NONCE_KEY',        'FrJ5504zNwjX30WAy96wYec80Rav2s0vogEPD8UMyTwsYSDJiyDaD3u0rdFqdNXg');
+define('AUTH_SALT',        'UHix72yILwgN8LfgMApxTVa8ISuWIh6o526m3K0IL26Hv4JW59v89Hw8H7DmzFuR');
+define('SECURE_AUTH_SALT', 'Vifaw68f42wFUxqMNApo7V9NiS1bREmf6rr3j5SaHUA08GTgHKcMo9TuUCfDUBNG');
+define('LOGGED_IN_SALT',   'oXQC212GDbGVWQ2DbjA8wEsuHiFEawu8QzEBSBTxQRC7ErdaT0iyraYP9fbgAI7b');
+define('NONCE_SALT',       'dtJtCNAoU0LosH57wYULXmEqyKpTE5pC42cmgpi9AVSYji8xiNF5xhWGP29dFgMz');
