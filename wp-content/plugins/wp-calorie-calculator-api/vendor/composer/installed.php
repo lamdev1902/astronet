08eeac69215b9ebada4336b10f51bb889a113314
truclam1902 <nguyenhongtruclam1@gmail.com>
2024-01-13 08:16:24 +0700

    composer

---
+++ wp-content/plugins/wp-calorie-calculator-api/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'calculator/api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1a3504fea94ee2c4fe4455d6fbef9d47cd36aaac',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'calculator/api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1a3504fea94ee2c4fe4455d6fbef9d47cd36aaac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
