f37adcf25ba83011e31f5ef036e7b7fe2df90d11
truclam1902 <nguyenhongtruclam1@gmail.com>
2024-02-19 15:53:01 +0700

    test customer shortcode

--- wp-content/themes/spotlyte/functions.php
+++ wp-content/themes/spotlyte/functions.php
@@ -1417,8 +1417,7 @@ function reviews_shortcode($atts) {
     ob_start();
     include(plugin_dir_path(__FILE__) . 'template/review.php');
 	$rt = ob_get_clean();
-	wp_enqueue_style( 'review-css', get_template_directory_uri() . '/assets/css/review.css','','1.0.0');
-	wp_enqueue_script( 'review-js', get_template_directory_uri() . '/assets/js/review-validate.js','','1.0.0');
+	
     return $rt;
 }
 
