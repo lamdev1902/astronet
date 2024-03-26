6e1425c6f36fe67090d7be0fe37420953004db5b
truclam1902 <nguyenhongtruclam1@gmail.com>
2024-03-26 11:47:47 +0700

    test quiz shortcode v7

--- wp-content/themes/spotlyte/shortcode/calorie/quiz-shortcode.php
+++ wp-content/themes/spotlyte/shortcode/calorie/quiz-shortcode.php
@@ -2,7 +2,6 @@
 
 function quiz_style() {
 	wp_enqueue_style( 'quiz-css', get_template_directory_uri() . '/shortcode/calorie/assets/css/quiz.css', '', '1.0.0');
-	wp_enqueue_script( 'quiz-js', get_template_directory_uri() . '/shortcode/calorie/assets/js/quiz-plugin.js','','1.0.0');
 }
 
 add_action('init', 'quiz_style');
